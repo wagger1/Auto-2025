8783d66cc15ba4050f72ca0255d22f3bc98ff300
Akhil.A.S <119261400+wagger1@users.noreply.github.com>
2025-05-19 15:00:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import asyncio
+import logging
 from os import environ
 from pyrogram import Client, filters, idle
 from pyrogram import utils as pyroutils
 
 pyroutils.MIN_CHAT_ID = -999999999999
 pyroutils.MIN_CHANNEL_ID = -100999999999999
+from aiohttp import web
+from webcode import web_server  
+logging.getLogger("asyncio").setLevel(logging.CRITICAL -1)
 
+PORT = environ.get("PORT", "8080")
 API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
@@ -29,13 +34,27 @@ User = Client(name="user-account",
               )
 
 
-Bot = Client(name="auto-delete",
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+             name="auto-delete",
              api_id=API_ID,
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
              workers=300
              )
+    async def start(self):
+        await super().start()
+        print("Bot oombi!")
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot stopped. Bye.")
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
@@ -43,12 +62,14 @@ async def start(bot, message):
 
 @User.on_message(filters.chat(GROUPS))
 async def delete(user, message):
+    chat = message.chat.id
+    m = message.id
     try:
        if message.from_user.id in ADMINS:
           return
        else:
           await asyncio.sleep(TIME)
-          await Bot.delete_messages(message.chat.id, message.id)
+          await user.delete_messages(chat_id=chat, message_ids=m)
     except Exception as e:
        print(e)
        

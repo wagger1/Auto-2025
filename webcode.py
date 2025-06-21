1d8a78ab19f91b5bdc91c493a8889c46bb2c2e77
Akhil.A.S <119261400+wagger1@users.noreply.github.com>
2025-06-21 12:40:17 +0530

    Update webcode.py

--- webcode.py
+++ webcode.py
@@ -1,13 +1,6 @@
-from aiohttp import web
+from flask import Flask
+app = Flask(__name__)
 
-routes = web.RouteTableDef()
-
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response("MATRIX")
-
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
+@app.route('/')
+def home():
+    return "Bot is alive!", 200
